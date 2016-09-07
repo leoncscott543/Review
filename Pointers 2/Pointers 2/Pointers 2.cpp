ed185b00d4eee3bebd319a8b11aab33fef42d766
Leon Scott <leoncscott543@live.com>
2016-09-07 12:48:06 -0400

    more comments
    hip hip, horray

--- Pointers 2/Pointers 2/Pointers 2.cpp
+++ Pointers 2/Pointers 2/Pointers 2.cpp
@@ -7,37 +7,39 @@
 using namespace std;
 
 // here comes the func, this is sweet
-int add(int b)
+void add(int b)
 {
-	cout << endl << b;
-	int *p2;
-	p2 = &b;
+	cout << endl << b; // print the value of b at the beginning of every progression through the function
+	int *p2; // initialize the pointer variable p2
+	p2 = &b; // point p2 to b's address
 
-	*p2 = *p2+1;
+	*p2 = *p2+1; // adding 1 to b's value every time we progress through the function
 
-		if (*p2 < 21)
+		if (*p2 < 21) // this is a recursive conditinoal statement 
 		{
-			add(b);
+			add(b); // call the function add and pass the current value for b
 		}
 		else
-			return NULL;
+			return; // return nothing at the end of the loop 
 }
+
 int main()
 {
-	int a = 10;
+	int a = 10; // create and set varaible a to 10
 
-	int *p;
+	int *p; // creating a pointer variable aptley called "p"
 
-	p = &a;
+	p = &a; // point p to a's address. 
 
-	cout << "Dereferenced pointer: " << *p;
-	cout << endl << endl << "Address being pointed to: " << p;
-	cout << endl << endl << "Address of the pointer itself: " << &p;
+	cout << "Dereferenced pointer: " << *p; // printing the value p is pointing to
+	cout << endl << endl << "Address being pointed to: " << p; // print address being pointed to by p
+	cout << endl << endl << "Address of the pointer itself: " << &p; // print pointer p's address
 
-	cout << endl << endl << "Adding one to the pointer: ";
+	cout << endl << endl << "Adding one to the pointer: "; // print'n stuff
 
-	add(a);
+	add(a); // calling the add() function we made above, and passing varaible (a) 
 
+	
 	char f;
 	cin >> f;
     return 0;
